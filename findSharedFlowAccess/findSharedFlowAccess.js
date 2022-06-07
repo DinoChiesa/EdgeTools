c770c19d0681fe3630afaa656dd45b87ec751aba
Dino Chiesa <dchiesa@google.com>
2022-06-07 14:22:55 -0700

    update findSharedFlowAccess to support X/hybrid

--- findSharedFlowAccess/findSharedFlowAccess.js
+++ findSharedFlowAccess/findSharedFlowAccess.js
@@ -1,104 +1,83 @@
 #! /usr/local/bin/node
-/*jslint node:true */
 // findSharedFlowAccess.js
 // ------------------------------------------------------------------
-// in Apigee Edge, find all policies in all proxies that reference a SharedFlow
+// in Apigee, find all policies in all proxies that reference a SharedFlow
 //
-// last saved: <2020-May-29 16:55:21>
-/* jshint esversion:9 */
+// last saved: <2022-June-07 14:21:58>
+/* jshint node:true, esversion:9, strict:implied */
 
-var fs = require('fs'),
-    async = require('async'),
-    edgejs = require('apigee-edge-js'),
-    common = edgejs.utility,
-    apigeeEdge = edgejs.edge,
-    sprintf = require('sprintf-js').sprintf,
-    Getopt = require('node-getopt'),
-    version = '2020-05-26:15:57:00',
-    getopt = new Getopt(common.commonOptions.concat([
-      ['s' , 'sharedflow=ARG', 'Optional. SharedFlow name to find.'],
-      ['d' , 'deployed', 'Optional. Search only deployed proxies.'],
-      ['e' , 'environment=ARG', 'Optional. Search only deployed proxies in environment, -d must be specified']
-    ])).bindHelp();
+const fs = require('fs'),
+      path = require('path'),
+      util = require('util'),
+      tmp      = require('tmp-promise'),
+      AdmZip   = require('adm-zip'),
+      DOM      = require('@xmldom/xmldom').DOMParser,
+      xpath    = require('xpath'),
+      apigeejs = require('apigee-edge-js'),
+      common = apigeejs.utility,
+      apigee = apigeejs.apigee,
+      Getopt = require('node-getopt'),
+      version = '20220607-0930',
+      getopt = new Getopt(common.commonOptions.concat([
+      ['s' , 'sharedflow=ARG', 'Optional. SharedFlow name to find. default: find any sharedflow reference.'],
+      ['' , 'deployed', 'Optional. Search only proxies that are deployed to any environment.'],
+      ['' , 'environment=ARG', 'Optional. Search only proxies that are deployed to the specified environment.'],
+      ['' , 'regex=ARG', 'Optional. Search only proxies with names matching a regular expression.'],
+      ])).bindHelp();
 
 // ========================================================
 
+function processZipBundle(namerev, zipfile) {
+  // temporarily unzip the file and then scan the dir
+
+    let zip = new AdmZip(zipfile),
+        zipEntries = zip.getEntries(),
+        policyFileRe = new RegExp('^apiproxy/policies/[^/]+\\.xml$'),
+        policies = zipEntries.filter( entry => entry.entryName.match(policyFileRe));
+
+    let flowCallouts = policies.filter(entry => {
+        let data = entry.getData().toString('utf8'),
+            doc = new DOM().parseFromString(data),
+            policyType = xpath.select('/*', doc)[0].tagName;
+
+        let found = (policyType == 'FlowCallout');
+        if (found && opt.options.sharedflow) {
+          let target = xpath.select('/FlowCallout/SharedFlowBundle', doc)[0].value;
+          found = (target == opt.options.sharedflow);
+        }
+        return found;
+      });
+  return flowCallouts.length ?
+    {
+      ...namerev,
+      flowCalloutPolicies: flowCallouts.map(fc => fc.name)
+    } :
+    null;
+
+}
+
+function getExporter(org, tmpdir) {
+  return ({name, revision}) => {
+    return org.proxies.export({name:name, revision:revision})
+      .then( result => {
+        let fqfn = path.join(tmpdir, result.filename);
+      fs.writeFileSync(fqfn, result.buffer);
+      // if (opt.options.verbose) {
+      //   common.logWrite('export ok file: %s', fqfn);
+      // }
+      return fqfn;
+    });
+  };
+}
+
 console.log(
-  'Apigee Edge SharedFlow check tool, version: ' + version + '\n' +
-    'Node.js ' + process.version + '\n');
+  `Apigee SharedFlow check tool, version: ${version}\n` +
+    `Node.js ${process.version}\n`);
 
 common.logWrite('start');
 
 // process.argv array starts with 'node' and 'scriptname.js'
-var opt = getopt.parse(process.argv.slice(2));
-
-function handleError(e) {
-    if (e) {
-      console.log(e);
-      console.log(e.stack);
-      process.exit(1);
-    }
-}
-
-function examineOnePolicy(org, options) {
-  return function(policyName, callback) {
-    org.proxies.getPoliciesForRevision({...options, policy:policyName}, function(e, result) {
-      //
-      handleError(e);
-      // return true if SharedFlow and if the sharedFlowBundle is as specified
-      var boolResult = (result.policyType == 'FlowCalloutBean') &&
-        ( ! opt.options.sharedflow || (opt.options.sharedflow == result.sharedFlowBundle) );
-      callback(null, boolResult);
-    });
-  };
-}
-
-function getOneRevision (org, proxyName) {
-  return function (revision, callback) {
-    var options = {name:proxyName, revision:revision};
-    org.proxies.getPoliciesForRevision(options, function(e, result){
-      async.filterSeries(result, examineOnePolicy(org, options), function(err, results) {
-        // results now equals an array of the SharedFlow policies in this revision
-        callback(null, results.map(function(elt){ return sprintf('apis/%s/revisions/%s/policies/%s', proxyName, revision, elt); }));
-      });
-    });
-  };
-}
-
-function doneAllRevisions(proxyName, callback) {
-  return function(e, results) {
-    handleError(e);
-    // results is an array of arrays
-    var flattened = [].concat.apply([], results);
-    common.logWrite('proxy: '+ proxyName + ' ' + JSON.stringify(flattened));
-    callback(null, flattened);
-  };
-}
-
-function doneAllProxies(e, results) {
-  handleError(e);
-  var flattened = [].concat.apply([], results);
-  common.logWrite('matching SharedFlow policies: ' + JSON.stringify(flattened));
-}
-
-function analyzeOneProxy(org) {
-  return function(proxyName, callback) {
-    org.proxies.get({ name: proxyName }, function(e, result){
-      handleError(e);
-      async.mapSeries(result.revision, getOneRevision(org, proxyName), doneAllRevisions(proxyName, callback));
-    });
-  };
-}
-
-function analyzeOneProxyRevision(org) {
-  return function(proxyNameRevision, callback) {
-    org.proxies.get({ name: proxyNameRevision.name, revision: proxyNameRevision.revision }, function(e, result){
-      handleError(e);
-      async.mapSeries(result.revision, getOneRevision(org, proxyNameRevision.name), doneAllRevisions(proxyNameRevision.name, callback));
-    });
-  };
-}
-
+let opt = getopt.parse(process.argv.slice(2));
 
 common.verifyCommonRequiredParameters(opt.options, getopt);
 
@@ -110,32 +89,56 @@ var options = {
       verbosity: opt.options.verbose || 0
     };
 
-apigeeEdge.connect(options, function(e, org){
-  if (e) {
-    common.logWrite(JSON.stringify(e, null, 2));
-    //console.log(e.stack);
-    process.exit(1);
-  }
-
-  if( opt.options.deployed ) {
-    org.proxies.getDeployments({}, function(e, deployments) {
-      let proxies = [];
-      deployments.environment.forEach( e => {
-          if( !opt.options.environment || (opt.options.environment === e.name)) {
-            e.aPIProxy.forEach( proxy => {
-              proxy.revision.forEach( r => {
-                if (proxies.findIndex( t =>  t.name === proxy.name && t.revision === r.name) == -1) {
-                  proxies.push({name:proxy.name, revision:r.name});
-                }
-              });
-            });
-          }
+apigee.connect(common.optToOptions(opt))
+  .then(org => {
+    let p = null;
+    if( opt.options.deployed || opt.options.environment) {
+      let opts = opt.options.environment ? {environment:opt.options.environment} : {};
+      p = org.proxies.getDeployments(opts)
+        .then(r =>
+              r.deployments.map(d => ({name: d.apiProxy, revision:d.revision})));
+      // array of {name, revision}
+      if (opt.options.regex) {
+        // filter on name
+        p = p
+          .then(pairs => pairs.filter( ({name}) => name.match(new RegExp(opt.options.regex))));
+      }
+    }
+    else {
+      // get all proxies
+      p = org.proxies.get()
+        .then(r => r.proxies.map(p => p.name));
+      if (opt.options.regex) {
+        // filter on name
+        let re = new RegExp(opt.options.regex);
+        p = p
+          .then(proxies => proxies.filter( name => name.match(re)));
+      }
+      // get latest revision of each
+      p = p.then(a => {
+          let reducer = (p, name) =>
+            p.then(a =>
+                 org.proxies.get({name})
+                 .then( ({revision}) => [...a, {name, revision:Math.max.apply(Math,revision.map(c => Number(c)))}]));
+          return a.reduce(reducer, Promise.resolve([])); // array of {name, revision}
         });
-      async.mapSeries(proxies, analyzeOneProxyRevision(org), doneAllProxies);
-    });
-  } else {
-    org.proxies.get({}, function(e, proxies) {
-      async.mapSeries(proxies, analyzeOneProxy(org), doneAllProxies);
-    });
-  }
-});
+    }
+
+    return p
+      .then(pairs =>
+            ({tmpdir:tmp.dirSync({unsafeCleanup:true, prefix: 'findSharedFlowAccess'}), pairs}))
+      .then(({tmpdir, pairs}) => {
+        let exportOne = getExporter(org, tmpdir.name);
+        let reducer = (p, namerev) =>
+        p.then(a =>
+               exportOne(namerev)
+               .then(zipFilename =>
+                     [...a, processZipBundle(namerev, zipFilename)]));
+        return pairs.reduce(reducer, Promise.resolve([])) // reduce to those FC, maybe to specific SharedFlow
+          .then(r => r.filter(r => !!r))
+          .then(r => (tmpdir.removeCallback(), r));
+      });
+  })
+  .then(r => console.log(JSON.stringify(r, null, 2)))
+
+  .catch( e => console.log('while executing, error: ' + util.format(e)) );
